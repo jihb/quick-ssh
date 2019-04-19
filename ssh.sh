cf08a61154b088b76777a75d6288ba76c8090268
jihuabin <jihuabin@baidu.com>
2019-04-19 11:28:16 +0800

    fix

--- ssh.sh
+++ ssh.sh
@@ -3,9 +3,9 @@ HOME_DIR=${HOME}/quick-ssh
 EXPECT=${HOME_DIR}/expect.sh
 CONFIG_LIST=${HOME_DIR}/user.conf
 echo ${HOME}
-echo "\n\035[32m-------Select a host you need login-------\033[0m"
+echo "\n\035[32m-------Select a server you need login-------\033[0m"
 i=0
-cat $CONFIG_LIST| awk 'NR>2' | while read user  host  pwd
+cat $CONFIG_LIST| awk 'NR>2' | while read user  server  pwd
 do
     let i=$i+1
     echo "\033[33m[$i]:\033[0m\033[37m$user\033[0m\033[35m<$server>\033[0m"
@@ -18,6 +18,4 @@ user=$(cat $CONFIG_LIST| awk 'NR=='$line' {print $1}')
 host=$(cat $CONFIG_LIST| awk 'NR=='$line' {print $2}')
 pwd=$(cat $CONFIG_LIST| awk 'NR=='$line' {print $3}')
 
-echo $pwd
-#echo "\033[37m$user\033[0m\033[35m<$host>\033[0m\033[35m<$pwd>\033[0m"
-#$EXPECT $user $host $pwd
+$EXPECT $user $host $pwd
